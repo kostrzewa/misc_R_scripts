b5f280df37a23224beec7fb52d2910a28ad2481f
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2013-05-15 14:10:30 +0200

    polish oriented plaquettes output

--- plot_oriented_plaquettes.R
+++ plot_oriented_plaquettes.R
@@ -1,4 +1,5 @@
 plot_oriented_plaquettes <- function(filename, start, end=-1) {
+  name <- strsplit(filename,split=c('/','.'))
   i_start <- start+1
   i_end <- end+1
   plaqdat <- read.table(filename)
@@ -25,7 +26,7 @@ plot_oriented_plaquettes <- function(filename, start, end=-1) {
       var(plaqdat$V7[i_start:i_end]) 
     )/(i_end-i_start)  )
 
-  par(mfrow=c(1,2))
+  par(mfrow=c(1,2),oma=c(0,0,1.5,0))
 
   labels <- c("TX","TY","TZ","XY","XZ","YZ") 
   plotwitherror(x=seq(1,6,1),y=orplaqs,dy=dorplaqs,ylab="oriented <P>",xlab="hyperplane",xaxt="n")
@@ -33,14 +34,20 @@ plot_oriented_plaquettes <- function(filename, start, end=-1) {
 
   ymax <- max(plaqdat$V2[i_start:i_end],plaqdat$V3[i_start:i_end],plaqdat$V4[i_start:i_end],plaqdat$V5[i_start:i_end],plaqdat$V6[i_start:i_end],plaqdat$V7[i_start:i_end])
   ymin <- min(plaqdat$V2[i_start:i_end],plaqdat$V3[i_start:i_end],plaqdat$V4[i_start:i_end],plaqdat$V5[i_start:i_end],plaqdat$V6[i_start:i_end],plaqdat$V7[i_start:i_end])
-  plot(t='l',ylab="oriented <P>",xlab=expression(t[HMC]),
+  
+  plot(t='p',ylab="oriented <P>",xlab=expression(t[HMC]),
     x=seq(start,i_end-1,1),
     y=plaqdat$V2[i_start:i_end],
-    ylim=c(ymin,ymax))
+    ylim=c(ymin,ymax),
+    pch='.')
 
-  lines(x=seq(start,i_end-1,1),y=plaqdat$V3[i_start:i_end],col="red")
-  lines(x=seq(start,i_end-1,1),y=plaqdat$V4[i_start:i_end],col="green")
-  lines(x=seq(start,i_end-1,1),y=plaqdat$V5[i_start:i_end],col="blue")
-  lines(x=seq(start,i_end-1,1),y=plaqdat$V6[i_start:i_end],col="cyan")
-  lines(x=seq(start,i_end-1,1),y=plaqdat$V7[i_start:i_end],col="magenta")
+  points(x=seq(start,i_end-1,1),y=plaqdat$V3[i_start:i_end],col="red",pch='.')
+  points(x=seq(start,i_end-1,1),y=plaqdat$V4[i_start:i_end],col="green",pch='.')
+  points(x=seq(start,i_end-1,1),y=plaqdat$V5[i_start:i_end],col="blue",pch='.')
+  points(x=seq(start,i_end-1,1),y=plaqdat$V6[i_start:i_end],col="cyan",pch='.')
+  points(x=seq(start,i_end-1,1),y=plaqdat$V7[i_start:i_end],col="magenta",pch='.')
+
+  title <- name[[1]][length(name[[1]])-1]
+  print(title)
+  mtext(title,outer=TRUE,cex=1.3)
 }
