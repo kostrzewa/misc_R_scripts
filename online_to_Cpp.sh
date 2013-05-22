d0fabd0396a41a2185f97af2d182a52308e2ad00
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2013-05-22 13:30:26 +0200

    skip over missing files

--- online_to_Cpp.sh
+++ online_to_Cpp.sh
@@ -7,5 +7,7 @@ fi
 
 for i in `seq ${1} ${2} ${3}`; do
   num=`printf "%06d" ${i}`
-  grep -e '^1  1' onlinemeas.${num} | awk '{print $3, $4}' > Cpp.data.${num}.online
+  if [ -e onlinemeas.${num} ]; then
+    grep -s -e '^1  1' onlinemeas.${num} | awk '{print $3, $4}' > Cpp.data.${num}.online
+  fi
 done
