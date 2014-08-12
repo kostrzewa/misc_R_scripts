4532be2536cec2be97e88b1df40a3d86821b1aa6
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2014-08-12 17:12:45 +0200

    plot_Force_timeseries: implement plot_forces function

--- plot_force_timeseries.R
+++ plot_force_timeseries.R
@@ -1,17 +1,42 @@
 source("~/code/R/misc_R_scripts/plot_timeseries.R")
-plot_forces <- function(path) {
-  files <- list.files(path,pattern="*.data")
+plot_forces <- function(path,skip=0,debug=FALSE) {
+  files <- list.files(path,pattern="*.force.data",full.names=T)
   for( i in seq(1,length(files))) {
+   
+    # extract some strings from the filename 
     print(files[i])
-    name <- strsplit(files[i],split=c("."),fixed=T)
+    filename <- strsplit(files[i],split=c("/"),fixed=T)
+    name <- filename[[length(filename)]][1]
+    monomial <- filename[[length(filename)]][ length( filename[[length(filename)]] ) ]
+    monomial <- strsplit(monomial,split=c("."), fixed=T)
+    monomial <- monomial[[1]][1]
+    if(debug) {
+      cat("Extracting name and monomial name from filename\n")
+      cat("Filename:", files[i], "\n")
+      cat("Name:", name, "\n")
+      cat("Monomial:", monomial, "\n")
+    }
+
     forcedat <- read.table(files[i])
     offset <- 0
     for( measure in c("avg","max") ) {
-      titletext <- sprintf("%s.%s.force",name[[1]][1],measure)
-      pdf.filename <- sprintf("%s.pdf",titletext)
-      plot_timeseries(dat=forcedat[100:max(forcedat[,1]),(3+offset)],trange=c(100,max(forcedat[,1])),ylab=paste(measure,"F^2"),pdf.filename=pdf.filename,
+      titletext <- sprintf("%s.%s.force",monomial,measure)
+      pdf.filename <- sprintf("%s.%s.pdf",titletext,name)
+      if(debug) {
+        cat("PDF filename:", pdf.filename, "\n")
+      }
+      plot_timeseries(dat=forcedat[skip:length(forcedat[,1]),(2+offset)],trange=c((skip),length(forcedat[,1])),ylab=paste(measure,"F^2"),pdf.filename=pdf.filename,
       name=paste(measure,"F^2"), plotsize=5,filelabel=titletext,titletext=titletext)
       offset <- offset + 1
     }
   }
+
+  if( Sys.which("pdfcat") != "" ) {
+    command <- sprintf("pdfcat forces.%s.pdf *.force.%s.pdf",name,name)
+    print(paste("calling",command))
+    system(command=command)
+  } else {
+    print("pdfcat not found, not concatenating plots!")
+  }
+  
 }
