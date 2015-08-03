4756cc2e48e6d9575291fa1bc4fc907672eed583
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2015-08-03 15:01:04 +0200

    plot_mpcac_v_kappa: simplify implementation, move finding of kappa_c to different routine

--- plot_mpcac_v_kappa.R
+++ plot_mpcac_v_kappa.R
@@ -8,135 +8,73 @@
 # with the corresponding values listed below, one set per row
 # colour is a string for the colour name such as "red" or "blue"
 
-plot_mpcac_v_kappa <- function(datafile,debug=F,sim=500,neg=TRUE,n.predict=5000,...)
+plot_mpcac_v_kappa <- function(datafile,debug=FALSE,fit=TRUE,sim=500,n.predict=2000,...)
 {
   pcacdat <- read.table(file=datafile,header=T,stringsAsFactors=FALSE,fill=FALSE)
   pcacdat <- cbind(oneov2k=1/(2*pcacdat$kappa),pcacdat)
   if(debug) {
     print(pcacdat)
   }
-
-  predict.newdata <- data.frame(oneov2k=seq(1/0.32,1/0.25,length.out=n.predict))
-
   
-  models <- list()
-  predictions <- list()
-  coefs.cov <- list()
-  coefs <- list()
-  kappa_c <- data.frame(lwr=c(),fit=c(),upr=c())
-
-  testpoint <- data.frame(x=0,y=0,dy=0)
-
-  # if we have negative and positive mpcac masses (at least two of either),
-  # we should do two fits because the negative and positive slopes are different
-  no_mpcac <- c(length(which(pcacdat$mpcac>0)),length(which(pcacdat$mpcac<0)) )
-  if(sum(no_mpcac)>=2){
-    l.rws <- list()
-    l.rws[[1]] <- 1:sum(no_mpcac)
-    if(no_mpcac[1]>=2 | no_mpcac[2]>=2){
-      l.rws[[1]] <- which(pcacdat$mpcac>0)
-      l.rws[[2]] <- which(pcacdat$mpcac<0)
-    }
-    
-    for( rws in l.rws ){
-      if( length(rws) < 2 ) next;
-      simdata <- data.frame(matrix(ncol=length(rws),nrow=sim))
-      for( k in 1:length(rws) ) {
-        simdata[,k] <- rnorm(n=sim,mean=pcacdat$mpcac[rws[k]],sd=pcacdat$dmpcac[rws[k]])
-      }
-      mpcacmod <- apply(X=simdata,MARGIN=1,
-                        FUN=function(x) {
-                              lm(mpcac~oneov2k,data=data.frame(mpcac=x,oneov2k=pcacdat$oneov2k[rws]),weights=1/(pcacdat$dmpcac[rws])^2)
-                            } )
-
-      coefs.tmp <- lapply(X=mpcacmod,FUN=function(x) { x$coefficients })
-      coefs.tmp <- array(data=unlist(coefs.tmp),dim=c(2,sim))
-
-      prediction <- lapply(X=mpcacmod,FUN=function(x) { predict(x,newdata=predict.newdata,interval="none") })
-
-      prediction <- array(data=unlist(prediction),dim=c(n.predict,sim))
-
-      models[[length(models)+1]] <- mpcacmod
-      predictions[[length(predictions)+1]] <- data.frame(val=apply(X=prediction,MARGIN=1,FUN=mean),err=apply(X=prediction,MARGIN=1,FUN=sd))
-      coefs[[length(coefs)+1]] <- apply(X=coefs.tmp,MARGIN=1,FUN=mean)
-      coefs.cov[[length(coefs.cov)+1]] <- cov(t(coefs.tmp))
-
-      kappa_estimate <- apply(X=prediction,MARGIN=2,FUN=function(x) { approx(x=x,y=predict.newdata$oneov2k,xout=0.0)$y } )
-      kappa_c <- rbind(kappa_c, quantile(kappa_estimate,probs=c(0.1573,0.5,0.8427) ) )
-    }
-  } 
-  if(nrow(kappa_c)>0) {
-    cat("Estimates of kappa_c\n")
-    kc <- 0.5/rev(kappa_c)
-    print(sprintf("k_c = %f (+%f -%f)", kc[,2], kc[,3]-kc[,2], kc[,2]-kc[,1]))
-    cat("\n")
-  }
-  
-  cat("Fit coefficients and the sqrt of their variance-covariance matrices\n")
-  for(idx in 1:length(coefs) ) {
-    print(coefs[[idx]])
-    print(sqrt(coefs.cov[[idx]]))
-  }
-  
-  par(family="Times")
-  
-  # set up plot area
-  plot(x=( 1/(2*pcacdat$kappa) ), y=pcacdat$mpcac,  
-    xlab=expression(paste("1/2",kappa)), ylab=expression(~am[PCAC]), col=pcacdat$colour, 
-    pch=pcacdat$pch+pcacdat$offsetpch, type='n', ... )
-  
-  if(nrow(kappa_c)>0){
-    cols <- c("magenta","cyan")
-    alpha.cols <- col2rgb(cols,alpha=TRUE)
-    alpha.cols[4,] <- 40
-    for( i in 1:nrow(kappa_c) ) {
-      poly.x <- c(predict.newdata$oneov2k,rev(predict.newdata$oneov2k))
-
-      coef.deriv <- matrix(nrow=2,ncol=length(predict.newdata$oneov2k))
-      coef.deriv[1,] <- 1
-      coef.deriv[2,] <- predict.newdata$oneov2k
-      yvar <- t(coef.deriv) %*% coefs.cov[[i]] %*% coef.deriv
-      dy <- sqrt(diag(yvar))
-
-      poly.y <- c(predictions[[i]]$val-dy,rev(predictions[[i]]$val+dy))
-      polygon(x=poly.x,y=poly.y,border=NA,
-              col=rgb(red=alpha.cols[1,i],green=alpha.cols[2,i],blue=alpha.cols[3,i],alpha=alpha.cols[4,i],maxColorValue=255))
-      points(y=0,x=kappa_c[i,2],col=cols[i],pch=16)
-      lines(x=predict.newdata$oneov2k,y=predictions[[i]]$val,col=cols[i])
+  simdata <- NULL
+  if(sim > 0) {
+    simdata <- data.frame(matrix(ncol=nrow(pcacdat),nrow=sim))
+    for( k in 1:nrow(pcacdat) ){
+      simdata[,k] <- rnorm(n=sim,mean=pcacdat$mpcac[k],sd=pcacdat$dmpcac[k])
     }
   }
-
-  abline(h=0,lty=2)
   
-  plotwitherror(x=( 1/(2*pcacdat$kappa) ), y=pcacdat$mpcac, dy=pcacdat$dmpcac, 
-    xlab=expression(paste("1/2",kappa)), ylab=expression(~am[PCAC]), col=pcacdat$colour, 
-    pch=pcacdat$pch+pcacdat$offsetpch, rep=TRUE, ... )
+  lmodel <- NULL
+  lmodel.sim <- NULL
+  param.cov <- NULL
+  pred.y <- NULL
+  pred.x <- data.frame(oneov2k=seq(2.0,4.0,length.out=n.predict))
+  if(fit) {
+    lmodel <- lm(mpcac ~ oneov2k, data=pcacdat, weights=1/pcacdat$mpcac^2)
+    pred.y <- predict(lmodel,newdata=pred.x)
+    print(summary(lmodel))
+    if(sim>0){
+      lmodel.sim <- t(apply(X=simdata,MARGIN=1,
+                          FUN=function(x){ 
+                                           summary(lm(mpcac~oneov2k,data=data.frame(mpcac=x,oneov2k=pcacdat$oneov2k),weights=1/pcacdat$dmpcac^2))$coefficients[1:2]
+                                         }
+                         ) )
+      param.cov <- cov(lmodel.sim)
+      print(param.cov)
+    }
+  }
+  
+  tikzfiles <- tikz.init(basename="mpcac_v_kappa",width=3.4,height=3.4,lwdUnit=0.8)
+  
+  par(mgp=c(3,0.3,0))
+  # prepare plot area
+  plotwitherror(x=pcacdat$oneov2k, y=pcacdat$mpcac, dy=pcacdat$dmpcac, 
+    xlab="", ylab="$ a m_\\mathrm{PCAC} $", col=pcacdat$colour, 
+    pch=pcacdat$pch+pcacdat$offsetpch,las=1,tck=0.02, ... )
+  mtext(side=1,text="$ 1/2\\kappa $",line=1.3)
+  abline(h=0,col="gray",lty=2)
+  if(fit) {
+    if(sim>0) {
+      dpar <- as.matrix(data.frame(dint=rep(1,times=n.predict),dslope=pred.x$oneov2k))
+      dy <- sqrt(diag( dpar %*% param.cov %*% t(dpar) ))
+      poly.x <- c(pred.x$oneov2k,rev(pred.x$oneov2k))
+      poly.y <- c(pred.y+dy,rev(pred.y-dy))
+      polygon(x=poly.x,y=poly.y,col="#0000FF55",border=NA)
+    }
+    lines(x=pred.x$oneov2k,y=pred.y)
+  }
+  plotwitherror(x=pcacdat$oneov2k, y=pcacdat$mpcac, dy=pcacdat$dmpcac, 
+                col=pcacdat$colour, pch=pcacdat$pch+pcacdat$offsetpch, rep=TRUE )
 
   # get plot boundaries
   lims <- par("usr")
 
-  # attempt to extract some coordinates for the legend 
-  # from variable parameter list
-  var_params <- list(...)
-  if(debug){
-    print(var_params)
-  }
+  legend( x="topleft", col=unique(pcacdat$colour), legend=sprintf("$a\\mu = %.4f$", unique( pcacdat$mu )), 
+          pch=15, bty='n', pt.cex=1.3 )
   
-  if( 'xlim' %in% names(var_params) ){
-    legend.xpos <- var_params$xlim[1]
-  } else {
-    print("legend x position NOT set")
-    legend.xpos <- 0
-  }
+  legend( x="bottomright", col="black", legend=sprintf("$L/a = %d$", unique( pcacdat$L ) ), 
+          pch=unique(pcacdat$pch), bty='n' )
 
-  if( 'ylim' %in% names(var_params) ){
-    legend.ypos <- var_params$ylim[2]
-  } else {
-    print("legend y position NOT set")
-    legend.ypos <- lims[4]
-  }
-
-  legend( x="topleft", col=unique(pcacdat$colour), legend=paste( "mu =", unique( pcacdat$mu ) ), 
-          pch=unique(pcacdat$pch+pcacdat$offsetpch), bty='n' )
+  tikz.finalize(tikzfiles)
   
 }
