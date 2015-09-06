6559158a96773e80afdb3d5fbe34a7e69338d809
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2015-09-06 13:19:43 +0200

    move all prop_error functions to data frames instead of lists

--- prop_error/binary_operations.R
+++ prop_error/binary_operations.R
@@ -1,7 +1,7 @@
 compute_ratio <- function(dividend,divisor,name=NA,debug=FALSE) {
-  rval <- list( val=dividend$val / divisor$val, 
-                 dval=sqrt( (dividend$dval/divisor$val)^2 + (divisor$dval*dividend$val/divisor$val^2)^2 ), 
-                 name=name )
+  rval <- data.frame( val=dividend$val / divisor$val, 
+                      dval=sqrt( (dividend$dval/divisor$val)^2 + (divisor$dval*dividend$val/divisor$val^2)^2 ), 
+                      name=name )
   if(debug) {
     print(sprintf("compute_ratio: %s",as.character(name)))
     print(rval)
@@ -10,9 +10,9 @@ compute_ratio <- function(dividend,divisor,name=NA,debug=FALSE) {
 }
 
 compute_product <- function(a,b,name=NA,debug=FALSE) {
-  rval <- list( val=a$val * b$val, 
-                 dval=sqrt( (a$dval*b$val)^2 + (b$dval*a$val)^2 ), 
-                 name=name )
+  rval <- data.frame( val=a$val * b$val, 
+                      dval=sqrt( (a$dval*b$val)^2 + (b$dval*a$val)^2 ), 
+                      name=name )
   if(debug) {
     print(sprintf("compute_product: %s",as.character(name)))
     print(rval)
